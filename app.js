867d7e5576c838beee035c151667eac4234ebbb1
Sébastien NOBILI <pipoprods@free.fr>
2015-06-21 22:21:39 +0200

    View transitions

--- app.js
+++ app.js
@@ -11,16 +11,49 @@ function () {
 
 		main: function () {
 			debug && console.log ('[__APPNAME__::router] Switching to main');
+			var _this = this;
 			require (['views/main'], function (View) {
-				var main = new View ({
-					el: $('body')
-				});
+				var main = new View ();
 				main.render ();
+				_this.goto (main);
 			});
+		}
+
+		// Handle view transition, switch from previous view (if any) to new view then drop previous view DOM contents
+		goto: function (view) {
+			var previous = this.currentPage || null;
+			var next = view;
+
+			// Transition direction (forward or backward)
+			var direction = app.direction ? app.direction : 'forward';
+
+			// View switching
+			if (previous) {
+				previous.$el.addClass (direction + ' leave');
+				setTimeout (function () {
+					previous.$el.remove ();
+					view.$el.removeClass (direction);
+				}, 500);
+				view.$el.addClass (direction);
+			}
+			$('body').append (view.$el.addClass ($('body').attr ('class')));
+			this.currentPage = next;
+
+			// Forget current transition direction
+			delete (app.direction);
+		},
+
+		back: function () {
+			app.direction = 'backward';
+			window.history.back ();
 		}
 	});
 
 	// Create router and start history manager
 	this.router = new Router ();
 	Backbone.history.start ();
+
+	// Make application widely available
+	window.app = this;
+	this.back  = this.router.back;
 });
