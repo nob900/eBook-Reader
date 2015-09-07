95492b3affb6d8b16bf77e0f71b0fa7363f89435
Sébastien NOBILI <pipoprods@free.fr>
2015-09-07 23:00:01 +0200

    _this bound to this

--- views/main.js
+++ views/main.js
@@ -9,6 +9,7 @@ function () {
 		id: 'main',
 		template: Handlebars.templates['main.html'],
 		render: function () {
+			var _this = this;
 			$(this.el).html (this.template ());
 			return (this);
 		}
